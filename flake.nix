05131c951355ae3b87f928613a5dda69cdbe4799
Gravity <alternativegravitational@protonmail.com>
2025-12-07 07:47:42 +0800

    try again again

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
           dwl-gravity = (pkgs.dwl.override { configH = ./config.h; }).overrideAttrs (old: {
             patches = (old.patches or [ ]) ++ [
               ./patches/bar/bar.patch
-              ./patches/bar-appicons/bar-appicons.patch
             ];
           });
         in
